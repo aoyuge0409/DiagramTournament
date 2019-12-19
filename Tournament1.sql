bff5c17a759b140699879ae4ba64db82515f6261
Andressa Yuge <yuriyuge04@gmail.com>
2019-12-18 19:46:04 -0500

    Update Tournament1.sql

--- Tournament1.sql
+++ Tournament1.sql
@@ -25,12 +25,21 @@ CREATE TABLE Tournaments.Tournament (
 	SportType varchar (40) NOT NULL,
 );
 
-CREATE TABLE Tournaments.Tournament (
-    TournamentId INT constraint PKTournament  PRIMARY KEY IDENTITY (1, 1),
-    TournamentName VARCHAR (50) NOT NULL,
-    DescriptionId VARCHAR (100) NOT NULL,
-    TournamentAddress VARCHAR (100) NOT NULL,
-    TournamentDate Date NOT NULL,
-	RegistrationCost Money NOT NULL,
-	SportType varchar (40) NOT NULL,
+
+CREATE TABLE Tournaments.Participant(
+	ParticipantId INT constraint PKEmployees PRIMARY KEY IDENTITY (1,1),
+	NameId VARCHAR (20) NOT NULL,
+	BirthDate DATE,
+	EmailAddress VARCHAR (60),
+	ParticipantPassword VARCHAR (20) NOT NULL,
+);
+
+
+CREATE TABLE Tournaments.Payment (
+    PaymentId INT constraint PKPayment  PRIMARY KEY IDENTITY (1, 1),
+    AmountPaid Money NOT NULL,
+    TournamentId int NOT NULL,
+    ParticipantId int NOT NULL,
+	FOREIGN KEY (TournamentId) REFERENCES Tournaments.Tournament(TournamentId),
+    FOREIGN KEY (ParticipantId) REFERENCES Tournaments.Participant(ParticipantId),
 );
